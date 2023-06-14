bf3d61c91795f8ef39441a279824eb1a25d1b31e
root <lucy.munyange@gmail.com>
2023-06-14 06:01:40 -0700

    added 5-number_keys.py and other .py files

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    num = 0
+    number_of_keys = list(a_dictionary.keys())
+
+    for i in number_of_keys:
+        num += 1
+        return (num)
