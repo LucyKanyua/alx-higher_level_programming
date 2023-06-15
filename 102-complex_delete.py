1910f421aef4abbe0efd8a9194a9be51deef3bd3
root <lucy.munyange@gmail.com>
2023-06-14 19:28:57 -0700

    added 100-weight_average.py and other .py files

---
+++ 102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    list_keys = list(a_ditionary keys())
+
+    for value_dic in list_keys:
+        if value == a_dictionary.get(value_dic):
+            del a_ditionary[value_di]
+
+            return (a_ditionary)
