bd4cd31ed0b86266ca123450885ce99036bf9147
root <lucy.munyange@gmail.com>
2023-06-27 01:12:24 -0700

    added 1-square.py and 2-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square: This module contains the definition of class Square"""
+
+
+class Square:
+    """A class defining a square
+    Attributes:
+    _size: length of the side of the square
+    """
+    def __init__(self, size):
+        """ Parameters:
+        size: length of one side of the square
+        """
+        self.__size = size
