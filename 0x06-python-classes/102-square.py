0228a0b03e2362e568f4f19a351e9cd488a3f14c
root <lucy.munyange@gmail.com>
2023-06-27 14:27:00 -0700

    modified 102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
         """Initialize a new square.
 
-        Args:;
+        Args:
         size (int): The size of the new square.
         """
         self.size = size
 
         @property
+        def size(self):
+            """Get/set the current size of the square."""
+            return (self.__size)
+
+        @size.setter
         def size(self, value):
             if not isinstance(value, int):
                 raise TypeError("size must be an integer")
             elif value < 0:
                 raise ValueError("size must be >= 0")
+
             self.__size = value
+
             def area(self):
-                """Return the current area of the Square."""
+                """Return the current area of the iSquare."""
                 return (self.__size * self.__size)
 
             def __eq__(self, other):
@@ -35,8 +43,8 @@ class Square:
                 """Define the < comparison to a Square."""
                 return self.area() < other.area()
 
-           def __le__(self, other):
-               """Define the <= comparison to a Square."""
+            def __le__(self, other):
+                """Define the <= comparison to a Square."""
                return self.area() <= other.area()
 
            def __gt__(self, other):
