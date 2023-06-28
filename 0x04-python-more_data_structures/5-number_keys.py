483ba7b86bf4d49d65a835c887d6aa6f6a3d90c5
root <lucy.munyange@gmail.com>
2023-06-28 06:15:42 -0700

    modified 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-def number_keys(a_dictionary):
-    num = 0
-    number_of_keys = list(a_dictionary.keys())
 
-    for i in number_of_keys:
-        num += 1
-        return (num)
+def number_keys(a_dictionary):
+    return len(a_dictionary)
